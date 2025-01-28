a6e342936e49c33d6d65f5530c51cf71e4384691
pundir8372 <pundirsahil320@gmail.com>
2025-01-28 10:55:03 +0530

    Added dockerfile

--- project-3/vite.config.js
+++ project-3/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Allows the server to be accessible externally
+    port: 5173,      // Specify the port (default is 5173)
+  },
+});
